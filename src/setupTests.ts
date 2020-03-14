de6e5f5cafba9d33a040d89e001910409e9f6069
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2020-03-14 17:19:44 +0800

    upgrade deps

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+// @ts-ignore
+import MutationObserver from '@sheerun/mutationobserver-shim';
+window.MutationObserver = MutationObserver;
